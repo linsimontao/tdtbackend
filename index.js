a3747404b29aee60c246ae5067f9ee8e59890b74
linsimontao <linsimontao@gmail.com>
2021-08-20 19:46:23 +0900

    adjust logic

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 const fetch = require('node-fetch');
-//const course100 = require('./Course100.json');
 const turf = require('@turf/turf');
 const dataPath = 'tokyo';
 const courseTokyo = require('./' + dataPath + '/Course.json');
-//const ptList100 = course100.features.map(pt => pt.geometry.coordinates);
 const ptList100 = courseTokyo.features.map(pt => pt.geometry.coordinates);
 const ls100 = turf.lineString(ptList100);
 
@@ -24,50 +22,44 @@ const finalPlayerPositionFile = 'players-locations.json';
 var prePlayerPositionMap = null;
 var curPlayerPositionMap = new Map();
 
+const tbc = require('./lib/turnback').tbc
+
+let playersRaw = null;
+
 // local test
-const playersRaw = require('./players-location.json');
+//playersRaw = require('./players-location.json');
 
 const main = async (evt) => {
-    // const response = await fetchPlayers();
-    // if (response.status === 200) {
-    //     const json = await response.json();
-    //     console.log('get data from pss', json);
-    //     if (json.result_code == '0') {
+    const response = await fetchPlayers();
+    if (response.status === 200) {
+        const json = await response.json();
+        console.log('get data from pss', json);
+        if (json.result_code == '0') {
+            playersRaw = json.result_detail;
+            if (playersRaw.length > 0) {
 
-    // get playersRaw from s3 dev-last-player-position/players-location.json
-    // let playersRaw = null;
-    // let ret = null;
-    // try {
-    //     ret = await download(lastPlayerPositionBucket, 'players-location.json');
-    //     playersRaw = JSON.parse(ret.Body);
-    //     console.log('players', players);
-    // } catch (err) {
-    //     console.log(err);
-    // }
+                //get previous players on T2
+                try {
+                    ret = await download(lastPlayerPositionBucket, lastPlayerPositionFile);
+                    const json = JSON.parse(ret.Body);
+                    prePlayerPositionMap = new Map(Object.entries(json));
+                } catch (err) {
+                    console.log(err);
+                }
 
-    //get previous players on T2
-    try {
-        ret = await download(lastPlayerPositionBucket, lastPlayerPositionFile);
-        const json = JSON.parse(ret.Body);
-        prePlayerPositionMap = new Map(Object.entries(json));
-    } catch (err) {
-        console.log(err);
+                const processed = processPlayers(playersRaw);
+                try {
+                    await upload(Object.fromEntries(curPlayerPositionMap), lastPlayerPositionBucket, lastPlayerPositionFile);
+                } catch (err) {
+                    console.log(err);
+                }
+
+                return await upload(processed, finalPlayerPositionBucket, finalPlayerPositionFile);
+            }
+        }
+        return response;
     }
-
-    const processed = processPlayers(playersRaw);
-    try {
-        await upload(Object.fromEntries(curPlayerPositionMap), lastPlayerPositionBucket, lastPlayerPositionFile);
-    } catch (err) {
-        console.log(err);
-    }
-
-    return await upload(processed, finalPlayerPositionBucket, finalPlayerPositionFile);
-
-    //       return upload(processed);
-    //     }
-    //     return response;
-    // }
-    // return response;
+    return response;
 }
 
 const upload = async (json, bucket, filename) => {
@@ -107,8 +99,8 @@ const download = async (bucket, filename) => {
 }
 
 const fetchPlayers = async () => {
-    const load_url = "https://cycling-st.demo.cycling.pssol.jp/api/mapbox/get_players_location"
-    const headers = { 'API_TOKEN': 'CywNKSJSA22YJH7664EPUfrKmL9LFKEZye_w_e3QAXC5G8fSSThp9yF9RHYKjnc95XChwdi4n7PNz8iE55FsipkZJ9DRmYA8i293' }
+    const load_url = "https://tdt2021.cycling.pssol.jp/api/mapbox/get_players_location";
+    const headers = { 'API_TOKEN': 'CywNKSJSA22YJH7664EPUfrKmL9LFKEZye_w_e3QAXC5G8fSSThp9yF9RHYKjnc95XChwdi4n7PNz8iE55FsipkZJ9DRmYA8i293' };
     const response = await fetch(load_url, { headers: headers });
     return response;
 }
@@ -118,7 +110,7 @@ const fetchLastPositions = async () => {
 }
 
 const processPlayers = (playersRaw) => {
-    const players = playersRaw.result_detail.map(player => {
+    const players = playersRaw.map(player => {
         const pt = turf.point([player.latlong.y, player.latlong.x]);
         const away = turf.pointToLineDistance(pt, ls100)
 
@@ -175,72 +167,18 @@ const processPlayers = (playersRaw) => {
     })
 
     // turn-back line correction
-    if (prePlayerPositionMap && prePlayerPositionMap.size > 0) {
-        corrected = corrected.map(player => {
-            // ignore players who are away from turn-back line
-            if (turf.pointToLineDistance(player.geometry.coordinates, t2) > 0) {
-                return player;
-            }
-            // ignore players whos's last point is not recorded
-            if (!prePlayerPositionMap.has(player.properties.player_id)) {
-                return player;
-            }
-            lastPosition = prePlayerPositionMap.get(player.properties.player_id);
-
-            // players'location, who are on the turn-back line
-            offsetLocation = turf.nearestPointOnLine(t2, player.geometry.coordinates);
-            console.log('offsetLocation', offsetLocation);
-
-            nearLocation = beforeT2 + offsetLocation.properties.location;
-            farLocation = beforeT2 + (t2Length - offsetLocation.properties.location);
-            console.log(nearLocation, farLocation, lastPosition.properties.distance);
-            // see https://docs.google.com/drawings/d/e/2PACX-1vQNEMbJTivqtWgfX8hm6hqAARZR-p53FpZ5Ud5Wktc17_AAMgJ8HCB5M8JdX9HPA5dtbAI9JMzTkLFC/pub?w=960&h=720
-            if (nearLocation > lastPosition.properties.distance) {
-                console.log('nearLocation:', nearLocation);
-                // p3-a
-                return {
-                    "type": "Feature",
-                    "properties": {
-                        ...player.properties,
-                        distance: nearLocation
-                    },
-                    "geometry": player.geometry
-                }
-            }
-
-            if (farLocation >= lastPosition.properties.distance) {
-                console.log('farLocation:', farLocation);
-                // p3-b
-                return {
-                    "type": "Feature",
-                    "properties": {
-                        ...player.properties,
-                        distance: farLocation
-                    },
-                    "geometry": player.geometry
-                }
-            }
-
-            // p3-c, should be ignored
-            return player;
-        })
-    }
-    // use this timestamp to correct location w/ Dummy data
-    // will be removed after real data received
-    const serverTime = new Date('2021-08-16 15:21:00+09:00');
+    corrected = tbc(corrected, prePlayerPositionMap, t2, beforeT2, t2Length)
 
     const nowString = Date().toLocaleString();
     const nowTS = Date.now();
     const final = corrected.map(player => {
         if (player.properties.away < 1) {
             const player_ts = Date.parse(player.properties.latlong_update_date + '+09:00')
-            let diffSec = (serverTime.getTime() - player_ts) / 1000;
+            let diffSec = (nowTS - player_ts) / 1000;
             if (diffSec < 0) {
                 diffSec = 0;
             }
-            // for dummy data need to provide a random speed(5m/s - 10m/s)
-            // will be removed with real data
-            const speed = Math.random() * 5 + 5;
+            const speed = player.properties.speed;
 
             // predict distance from last update to now, unit km
             const diffDist = (speed * diffSec) / 1000;
