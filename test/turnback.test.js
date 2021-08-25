481f9486e2f28bb62fef82ec4059d58ac3755d97
Yanpeng Lin <yanpeng.lin@mapbox.com>
2021-08-25 15:41:24 +0900

    update

--- test/turnback.test.js
+++ test/turnback.test.js
@@ -15,6 +15,9 @@ const beforeT2 = turf.length(
     turf.lineString(ls100.geometry.coordinates.slice(0, t2EntryPointIndex + 1)),
 )
 
+const realLog = require('./label_output3_player2.json')
+const fs = require('fs')
+
 // generating test context
 // expectedPattern = p1 | p2 | p3-a | p3-b
 const generateTest = (expectedPattern) => {
@@ -46,9 +49,7 @@ const generateTest = (expectedPattern) => {
 
     if (expectedPattern == 'p3-b') {
         pIdx = Math.floor(Math.random() * (t2EndIdx - t2MidIdx) + t2MidIdx)
-        lpIdx = Math.floor(
-            Math.random() * (t2MidIdx - t2StartIdx) + t2StartIdx,
-        )
+        lpIdx = Math.floor(Math.random() * (t2MidIdx - t2StartIdx) + t2StartIdx)
     }
 
     let lpPoint = turf.point(ls100.geometry.coordinates[lpIdx])
@@ -99,6 +100,7 @@ const generateTest = (expectedPattern) => {
     return { p: p, lp: lp }
 }
 
+/* 
 test('advanced auto tests', function (t) {
     const availabePatterns = ['p1', 'p2', 'p3-a', 'p3-b']
 
@@ -109,7 +111,7 @@ test('advanced auto tests', function (t) {
                 i +
                 ')------------------------------------------------------------------------------',
         )
-        
+
         context = generateTest(pattern)
         lpMap = new Map()
         lpMap.set('mbx_maru', context.lp)
@@ -119,16 +121,87 @@ test('advanced auto tests', function (t) {
         t.equal(1, actual.length, 'should length of response match first')
         if (pattern == 'p3-a') {
             t.true(
-                (t2Length / 2 - ( actual[0].properties.distance - beforeT2 )) > 0.05,
+                t2Length / 2 - (actual[0].properties.distance - beforeT2) >
+                    0.05,
                 'should be near',
             )
         }
         if (pattern == 'p3-b') {
             t.true(
-                (( actual[0].properties.distance - beforeT2 ) - t2Length / 2) > -0.05,
+                actual[0].properties.distance - beforeT2 - t2Length / 2 > -0.05,
                 'should be far',
             )
         }
     }
+    t.end()
+})
+*/
+
+const generateRealTest = (idx) => {
+    currentP = realLog[idx]
+    lastP = realLog[idx - 1]
+
+    // inject player_id
+    currentP.properties['player_id'] = 'mapbox'
+    lastP.properties['player_id'] = 'mapbox'
+
+    // console.log(currentP)
+    // console.log(lastP)
+
+    return { p: currentP, lp: lastP }
+}
+
+test('load real tests', function (t) {
+    context = generateRealTest(19)
+    lpMap = new Map()
+    lpMap.set('mapbox', context.lp)
+    console.log(context)
+    const actual = tbc([context.p], lpMap, t2, beforeT2, t2Length)
+    console.log(actual)
+
+    t.end()
+})
+
+test('line chunk test', function (t) {
+    var line = turf.lineString([
+        [139.737028, 35.656307],
+        [139.738223, 35.663208],
+        [139.746436, 35.67813],
+    ])
+
+    var chunk = turf.lineChunk(line, 0.1, { units: 'kilometers' })
+    var final = []
+    chunk.features.map((e) => {
+        // console.log(e.geometry.coordinates)
+        e.geometry.coordinates.map((ee) => {
+            final.push(ee)
+        })
+    })
+
+    // console.log(final)
+    t.end()
+})
+
+test('cut course test', function (t) {
+    var chunk = turf.lineChunk(ls100, 0.005, { units: 'kilometers' })
+    var final = []
+    chunk.features.map((e) => {
+        e.geometry.coordinates.map((ee) => {
+            final.push(ee)
+        })
+    })
+    var output = turf.lineString(final)
+    // console.log(output)
+    var jsonContent = JSON.stringify(output)
+
+    fs.writeFile('CourseTokyo_updated.json', jsonContent, 'utf8', function (err) {
+        if (err) {
+            console.log('An error occured while writing JSON Object to File.')
+            return console.log(err)
+        }
+
+        console.log('JSON file has been saved.')
+    })
+
     t.end()
 })
